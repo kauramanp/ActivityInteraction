3a69a68085d2fc601cf1519b57b8808e847a2393
kauramanp <33446281+kauramanp@users.noreply.github.com>
2022-10-08 18:52:22 +0530

    Initial commit

---
+++ app/src/main/java/com/aman/activityinteraction/ActivityInterface.kt
@@ -0,0 +1,8 @@
+package com.aman.activityinteraction
+
+interface ActivityInterface {
+    fun activityOneInterface( value: Int)
+}
+interface ActivitySecondInterface {
+    fun activitySecondInterface( value: Int)
+}
